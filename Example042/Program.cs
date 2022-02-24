d1830df8cc0e5e9cea3c12c1afaf61ff68fa8e66
Evgeniy <zadrod93@yandex.ru>
2022-02-24 18:35:11 +0300

    Задача номер 42

---
+++ Example042/Program.cs
@@ -0,0 +1,16 @@
+﻿// 42. Определить сколько цифр больше 0 введено с клавиатуры
+
+
+string getNumberStr = String.Empty;
+int getNumber = 1;
+int quan = 0;
+Console.WriteLine(" Введите числа, по окончании нажмите любую букву:");
+while (getNumber != 0  && getNumber > 0)
+{
+    getNumberStr = Console.ReadLine();
+    int.TryParse(getNumberStr, out getNumber);
+    quan++;
+    if(getNumber == 0 || getNumber < 0)
+    quan--;
+}
+Console.WriteLine(" Вы ввели " + quan + " чисел (числа) больше 0 ");
